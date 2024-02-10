3743e4f034ea7362aaa915082cc6c70d393dca15
nick <153327552+ftnick@users.noreply.github.com>
2024-02-11 09:20:00 +1100

    redo

--- assets/js/maintenance.js
+++ assets/js/maintenance.js
@@ -2,6 +2,21 @@ fetch("https://raw.githubusercontent.com/ftnick/Synthex-Roblox/main/maintenance.
     .then(response => response.text())
     .then(data => {
         if (data.trim() === "true") {
-            window.location.href = "maintenance.html";
+            localStorage.getItem('isLoggedIn');
+            localStorage.getItem("username");
+            if (localStorage.getItem('isLoggedIn') === "true") {
+                if (localStorage.getItem("username") === "admin") {
+                   bypass = confirm("You are logged in as ftnick, do you want to bypass the maintenance?");
+                   if (bypass) {
+                       window.location.href = "index.html";
+                   } else { 
+                       window.location.href = "maintenance.html";
+                   }
+                } else {
+                    window.location.href = "maintenance.html";
+                }
+            } else {
+                window.location.href = "maintenance.html";
+            }
         }
     });
